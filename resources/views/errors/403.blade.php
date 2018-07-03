9c52804bfe5b827daaef85d8694a2db454b57a89
tkt181lee <tkt181.lee@gmail.com>
2018-07-03 13:18:45 +0800

    安裝 laravel-debugbar 方便除錯

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,10 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 @section('content')
-    <h1>403 Forbidden.</h1>
-    <p>您沒有權限可以執行目前的動作喔！</p>
+  <h1>403 Forbidden.</h1>
+  <p>您沒有權限可以執行目前的動作喔！</p>
+  <?php
+    $default_error_message = "快滾回 <a href='".url('')."'>首頁</a> 吧！";
+  ?>
+  {!! isset($exception)? ($exception->getMessage()?$exception->getMessage():$default_error_message): $default_error_message
+  !!}
 @endsection
