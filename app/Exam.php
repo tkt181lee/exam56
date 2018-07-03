db74d42385df7f2adcaf0f4f910fcdd1ce7b9653
tkt181lee <tkt181.lee@gmail.com>
2018-07-03 14:23:47 +0800

    建立並執行題目的Model 及 migrate 檔案

---
+++ app/Exam.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Exam extends Model
+{
+    //
+    protected $table      = 'exams';
+    protected $primaryKey = 'id';
+}
