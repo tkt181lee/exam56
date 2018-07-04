81c884566af54f7628b721649e6576f95642acc2
tkt181lee <tkt181.lee@gmail.com>
2018-07-04 15:44:30 +0800

    讀出單一測驗

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
  */
+Route::pattern('id', '[0-9]+');
 
 Route::get('/', 'ExamController@index')->name('index');
 Route::get('/home', 'ExamController@index')->name('home.index');
 
 Auth::routes();
 
-Route::get('/exam', 'ExamController@index')->name('exam.index');
-Route::get('/exam/create', 'ExamController@create')->name('exam.create');
 Route::post('/exam', 'ExamController@store')->name('exam.store');
+Route::get('/exam', 'ExamController@index')->name('exam.index');
+Route::get('/exam/{id}', 'ExamController@show')->name('exam.show');
+Route::get('/exam/create', 'ExamController@create')->name('exam.create');
